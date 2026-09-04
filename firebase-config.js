5c503dc4d7b0e0850de88dc5eb8c8d5d0e03c026
VilhenaCH <vilhenacharles0@gmail.com>
2026-09-04 10:03:21 -0300

    Refactor Firebase configuration export
    Removed initialization and import statements for Firebase.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Configuração do Firebase — colada direto do console
+export const firebaseConfig = {
   apiKey: "AIzaSyBrRStfBY7GdXXx46CgY2BAFvWE56_Ixfc",
   authDomain: "files-f4650.firebaseapp.com",
   projectId: "files-f4650",
@@ -12,6 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "1018254178001",
   appId: "1:1018254178001:web:64207f77202b5264d3f7f0"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
